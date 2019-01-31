0ffd3c8b4b98ce5445619aa24e7e66845db4a432
xmartin46 <xmartin46@gmail.com>
2019-01-31 17:04:26 +0100

    Heroku

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql')
-const hp_config = require('./helpers/config')
+//const hp_config = require('./helpers/config')
 
-var db = mysql.createConnection({
+/*var db = mysql.createConnection({
   host: hp_config.DB_HOST,
   user: hp_config.DB_USER,
   password: hp_config.DB_PASSWORD,
   database: hp_config.DB_NAME
-});
+});*/
 
 module.exports = {
     port: process.env.PORT || 8080,
-    host: process.env.HOST || '0.0.0.0',
-    db
+    host: process.env.HOST || '0.0.0.0'
+    //db
 }
