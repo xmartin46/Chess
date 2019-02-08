9237c47f25c55e84659a8028eeaa436930048872
xmartin46 <xmartin46@gmail.com>
2019-02-08 23:01:59 +0100

    helper

---
+++ helpers/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    DB_PASSWORD: 'fe825cda5d82f752cc899deb39f226ef47ae37c0f00ecd4bc5fd1321afb2da87',
+    PORT: 8080,
+    DB_USER: 'ufsnrdoyeowowb',
+    DB_NAME: 'd5b16u14gh8916',
+    DB_HOST: 'ec2-54-217-208-105.eu-west-1.compute.amazonaws.com',
+    DB_PORT: 5432,
+    SESSION_SECRET: 'keyboard cat'
+}
